decaff7d4977e47fc85236964772c4c242f6b20d
Prelang Builder <builder@prelang.com>
2015-01-19 15:00:20 +0000

    Generated scaffolding for model 'Group' via Rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :groups
+
   resources :members
 
   devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords"}, skip: [:sessions, :registrations]
