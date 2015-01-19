61837a377bb68f48151419031aee82ae7a160476
Keenahn Jung <keenahn@gmail.com>
2015-01-19 07:17:56 -0800

    Added twilio

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '4.1.4'
+gem 'rails', '4.2'
 # Use postgresql as the database for Active Record
 gem 'pg'
 # Use SCSS for stylesheets
@@ -48,3 +48,5 @@ gem 'omniauth'
 gem 'omniauth-facebook'
 gem 'validates_formatting_of'
 gem 'tzinfo-data'
+gem 'twilio-ruby'
+gem 'thin'
---
+++ config/initializers/twilio.rb
@@ -0,0 +1,15 @@
+require "twilio-ruby"
+
+class TwilioClient
+  ACCOUNT_SID         = ENV["TWILIO_ACCOUNT_SID"]
+  AUTH_TOKEN          = ENV["TWILIO_AUTH_TOKEN"]
+  DEFAULT_FROM_NUMBER = ENV["TWILIO_DEFAULT_FROM_NUMBER"]
+
+  @@t = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN
+
+  def self.sms to_number, message, from_number = nil
+    from_number ||= DEFAULT_FROM_NUMBER
+    @@t.account.sms.messages.delay.create(from: from_number, to: to_number, body: message)
+  end
+end
+
