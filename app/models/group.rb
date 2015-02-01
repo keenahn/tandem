db4a54f9cdb334c5c853994cb15507be37e51c11
Keenahn Jung <keenahn@gmail.com>
2015-01-31 19:12:39 -0800

    Added to_s for group and member

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,9 @@ class Group < ActiveRecord::Base
   belongs_to :user
   has_and_belongs_to_many :members
   has_many :pairs
+
+  def to_s
+    name
+  end
+
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,9 @@
 class Member < ActiveRecord::Base
   has_and_belongs_to_many :groups
   has_and_belongs_to_many :pairs
+
+  def to_s
+    name
+  end
+
 end
