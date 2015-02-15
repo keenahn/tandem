446ea83850e3520d5be117d446fe74d7e3937da7
Keenahn Jung <keenahn@gmail.com>
2015-02-14 20:02:48 -0800

    Fixed up groups_controller spec

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe GroupsController do
 
@@ -6,86 +6,110 @@ describe GroupsController do
     @group = FactoryGirl.create(:group)
   end
 
-  describe "not Logged in" do
+  describe "Not Logged in" do
     it "GET new" do
       get :new
-      assert_response :redirect
+      expect(response.status).to eq(302)
     end
 
-    it "should show group" do
-      get :show, id: @group
-      assert_response :success
-    end
-
-    it "GET index" do
+    it "should not GET index" do
       get :index
-      assert_response :success
-      expect(assigns(:groups)).not_to be_nil
+      expect(response.status).to eq(302)
+      expect(assigns(:groups)).to be_nil
+    end
+
+    it "should not show group" do
+      get :show, id: @group
+      expect(response.status).to eq(302)
+    end
+
+    it "should not get edit" do
+      @g = FactoryGirl.create(:group, owner_id: 9999999)
+      get :edit, id: @g.id
+      expect(subject).to redirect_to(root_path)
     end
 
   end
 
   describe "Logged in" do
+
     login_user
 
-    it "GET new" do
-      get :new
-      assert_response :success
+    describe "has permission" do
+
+      it "GET new" do
+        get :new
+        expect(response.status).to eq(200)
+      end
+
+      it "should create group" do
+        c = Group.count
+        post :create, group: { description: @group.description, name: @group.name, owner_id: subject.current_user.id }
+        expect(Group.count).to equal(c + 1)
+        expect(subject).to redirect_to(group_path(assigns(:group)))
+      end
+
+      it "should get edit" do
+        @g = FactoryGirl.create(:group, owner_id: subject.current_user.id)
+        get :edit, id: @g.id
+        expect(response.status).to eq(200)
+      end
+
+      it "GET index" do
+        @g = FactoryGirl.create(:group, owner_id: subject.current_user.id)
+        get :index
+        expect(response.status).to eq(200)
+        expect(assigns(:groups)).not_to be_nil
+      end
+
+      it "should show group" do
+        @g = FactoryGirl.create(:group, owner_id: subject.current_user.id)
+        get :show, id: @g.id
+        expect(response.status).to eq(200)
+      end
+
+      it "should update group" do
+        @g = FactoryGirl.create(:group, owner_id: subject.current_user.id)
+        patch :update, id: @g, group: { description: @g.description + " derp", name: @g.name,  owner_id: subject.current_user.id }
+        expect(subject).to redirect_to(group_path(assigns(:group)))
+      end
+
+      it "should destroy group" do
+        @g = FactoryGirl.create(:group, owner_id: subject.current_user.id)
+        c = Group.count
+        delete :destroy, id: @g
+        expect(Group.count).to equal(c - 1)
+        expect(subject).to redirect_to(groups_path)
+      end
     end
 
-    it "should create group" do
-      c = Group.count
-      post :create, group: { description: @group.description, name: @group.name, user_id: @current_user.id }
-      expect(Group.count).to equal(c + 1)
-      expect(subject).to redirect_to(group_path(assigns(:group)))
+    describe "does not have permission" do
+      before :each do
+        @g = FactoryGirl.create(:group, owner_id: 9999999)
+      end
+
+      it "should not get edit" do
+        get :edit, id: @g.id
+        expect(subject).to redirect_to(root_path)
+      end
+
+      it "should not update" do
+        patch :update, id: @g, group: { description: @g.description + " derp", name: @g.name,  owner_id: subject.current_user.id }
+        expect(subject).to redirect_to(root_path)
+      end
+
+      it "should not show group" do
+        get :show, id: @group
+        expect(subject).to redirect_to(root_path)
+      end
+
+      it "should not destroy group" do
+        c = Group.count
+        delete :destroy, id: @g
+        expect(Group.count).to equal(c)
+        expect(subject).to redirect_to(root_path)
+      end
     end
-
-    it "should get edit" do
-      @g = FactoryGirl.create(:group, user_id: @current_user.id)
-      get :edit, id: @g.id
-      assert_response :success
-    end
-
-    it "should not get edit" do
-      @g = FactoryGirl.create(:group, user_id: 9999999)
-      get :edit, id: @g.id
-      assert_response :redirect
-    end
-
-
   end
 
 end
-
-
-
-# class GroupsControllerTest < ActionController::TestCase
-#   setup do
-#     @group = groups(:one)
-#   end
-
-
-
-#   test "should show group" do
-#     get :show, id: @group
-#     assert_response :success
-#   end
-
-#   test "should get edit" do
-#     get :edit, id: @group
-#     assert_response :success
-#   end
-
-#   test "should update group" do
-#     patch :update, id: @group, group: { description: @group.description, name: @group.name, user_id: @group.user_id }
-#     assert_redirected_to group_path(assigns(:group))
-#   end
-
-#   test "should destroy group" do
-#     assert_difference('Group.count', -1) do
-#       delete :destroy, id: @group
-#     end
-
-#     assert_redirected_to groups_path
-#   end
-# end
--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -4,8 +4,8 @@ module ControllerMacros
   def login_user
     before(:each) do
       @request.env["devise.mapping"] = Devise.mappings[:user]
-      @current_user = FactoryGirl.create(:user)
-      sign_in @current_user
+      user = FactoryGirl.create(:user)
+      sign_in user
     end
   end
 
