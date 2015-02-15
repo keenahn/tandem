46f656c18dfe3abe493e476dea57bb90c00686a3
Keenahn Jung <keenahn@gmail.com>
2015-02-15 12:15:16 -0800

    Cleaned up groups controller spec to use let

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -2,11 +2,9 @@ require "spec_helper"
 
 describe GroupsController do
 
-  before :all do
-    @group = FactoryGirl.create(:group)
-  end
-
   describe "Not Logged in" do
+    let(:g) { FactoryGirl.create(:group) }
+
     it "GET new" do
       get :new
       expect(response.status).to eq(302)
@@ -19,16 +17,14 @@ describe GroupsController do
     end
 
     it "should not show group" do
-      get :show, id: @group
+      get :show, id: g
       expect(response.status).to eq(302)
     end
 
     it "should not get edit" do
-      @g = FactoryGirl.create(:group, owner_id: 9999999)
-      get :edit, id: @g.id
+      get :edit, id: g.id
       expect(subject).to redirect_to(root_path)
     end
-
   end
 
   describe "Logged in" do
@@ -37,6 +33,8 @@ describe GroupsController do
 
     describe "has permission" do
 
+      let(:g) { FactoryGirl.create(:group, owner_id: subject.current_user.id) }
+
       it "GET new" do
         get :new
         expect(response.status).to eq(200)
@@ -44,69 +42,64 @@ describe GroupsController do
 
       it "should create group" do
         c = Group.count
-        post :create, group: { description: @group.description, name: @group.name, owner_id: subject.current_user.id }
+        gr = FactoryGirl.build(:group)
+        post :create, group: { description: gr.description , name: gr.name, owner_id: subject.current_user.id }
         expect(Group.count).to equal(c + 1)
         expect(subject).to redirect_to(group_path(assigns(:group)))
       end
 
       it "should get edit" do
-        @g = FactoryGirl.create(:group, owner_id: subject.current_user.id)
-        get :edit, id: @g.id
+        get :edit, id: g.id
         expect(response.status).to eq(200)
       end
 
       it "GET index" do
-        @g = FactoryGirl.create(:group, owner_id: subject.current_user.id)
         get :index
         expect(response.status).to eq(200)
         expect(assigns(:groups)).not_to be_nil
       end
 
       it "should show group" do
-        @g = FactoryGirl.create(:group, owner_id: subject.current_user.id)
-        get :show, id: @g.id
+        get :show, id: g.id
         expect(response.status).to eq(200)
       end
 
       it "should update group" do
-        @g = FactoryGirl.create(:group, owner_id: subject.current_user.id)
-        patch :update, id: @g, group: { description: @g.description + " derp", name: @g.name,  owner_id: subject.current_user.id }
+        patch :update, id: g, group: { description: g.description + " derp", name: g.name,  owner_id: subject.current_user.id }
         expect(subject).to redirect_to(group_path(assigns(:group)))
       end
 
       it "should destroy group" do
-        @g = FactoryGirl.create(:group, owner_id: subject.current_user.id)
-        c = Group.count
-        delete :destroy, id: @g
-        expect(Group.count).to equal(c - 1)
+        expect(Group.find_by_id g.id).to_not be_nil
+        delete :destroy, id: g.id
+        expect(Group.find_by_id g.id).to be_nil
         expect(subject).to redirect_to(groups_path)
       end
     end
 
     describe "does not have permission" do
-      before :each do
-        @g = FactoryGirl.create(:group, owner_id: 9999999)
-      end
+
+      let(:g) { FactoryGirl.create(:group, owner_id: 9999999) }
 
       it "should not get edit" do
-        get :edit, id: @g.id
+        get :edit, id: g.id
         expect(subject).to redirect_to(root_path)
       end
 
       it "should not update" do
-        patch :update, id: @g, group: { description: @g.description + " derp", name: @g.name,  owner_id: subject.current_user.id }
+        patch :update, id: g, group: { description: g.description + " derp", name: g.name,  owner_id: subject.current_user.id }
         expect(subject).to redirect_to(root_path)
       end
 
       it "should not show group" do
-        get :show, id: @group
+        get :show, id: g.id
         expect(subject).to redirect_to(root_path)
       end
 
       it "should not destroy group" do
         c = Group.count
-        delete :destroy, id: @g
-        expect(Group.count).to equal(c)
+        delete :destroy, id: g.id
+        expect(Group.find_by_id g.id).to_not be_nil
         expect(subject).to redirect_to(root_path)
       end
     end
