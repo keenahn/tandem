74e9a2f1f8c0119a7842773e8a47640836b287f4
Keenahn Jung <keenahn@gmail.com>
2015-06-21 21:01:55 -0700

    Added Sms.to_s, spaced out multiple Sms messages

--- app/models/sms.rb
+++ app/models/sms.rb
@@ -2,6 +2,9 @@
 class Sms < ActiveRecord::Base
   include ActiveSupport::Configurable
 
+  MAX_LENGTH = 160
+  DELAY_SECONDS = 2 # between multiple messages
+
   validates :to_id,   presence: true
   validates :from_id, presence: true
   validates :message, presence: true
@@ -15,7 +18,6 @@ class Sms < ActiveRecord::Base
   after_validation :clean_params
   # after_create :delay_send_sms
 
-  MAX_LENGTH = 160
 
   def dry_run?
     Sms.config.dry_run
@@ -23,9 +25,15 @@ class Sms < ActiveRecord::Base
 
   def self.create_and_send p
     messages = Array(p.delete(:message)) # will Array'ize single elements
+    # p[:message] = messages.join("\n\n")
+    # s = create(p)
+    # s.delay_send_sms
+    messages = Array(p.delete(:message)) # will Array'ize single elements
+    wait = 0
     messages.each{|m|
       s = create p.merge({message: m})
-      s.delay_send_sms
+      s.delay_send_sms(wait)
+      wait += DELAY_SECONDS
     }
 
   end
@@ -35,9 +43,13 @@ class Sms < ActiveRecord::Base
     TwilioClient.sms to_number, message
   end
 
-  def delay_send_sms
-    return Rails.logger.info(inspect) if dry_run?
-    SendSmsJob.perform_later self
+  def delay_send_sms wait = 0
+    return puts(self) && Rails.logger.info(self) if dry_run?
+    SendSmsJob.set(wait: wait.second).perform_later(self)
+  end
+
+  def to_s
+    "from: #{from_number} to: #{to_number} msg: #{message}"
   end
 
   private
