646f49e17d1cf5c809b378f562e4497b65d15d67
Keenahn Jung <keenahn@gmail.com>
2015-06-21 21:02:17 -0700

    Added gender to member factory

--- spec/factories/members_factory.rb
+++ spec/factories/members_factory.rb
@@ -3,5 +3,6 @@ FactoryGirl.define do
     name { Faker::Name.name }
     phone_number { Phoner::Phone.parse(Faker::PhoneNumber.cell_phone).to_s }
     active true
+    gender { ["male", "female", "neutral"].sample }
   end
 end
